5577e389f1b5f4d7d0c5e2449ce40488a826c706
cuong.nq <cuong.nq@csell.com.vn>
2020-08-27 14:28:59 +0700

    code: update css view order

--- src/pages/Order/OrderProcessing/index.js
+++ src/pages/Order/OrderProcessing/index.js
@@ -1,22 +1,65 @@
 import React, { useState, useEffect } from "react";
 import Button from "../../../components/Buttons";
 import Card from "../../../components/Cards";
-import Tables from "../../../components/Tables";
 import Item from "../../../components/Items";
 import Input from "../../../components/Input";
 import Radio from "../../../components/Radio";
-import InputGroup from 'react-bootstrap/InputGroup';
 import OrderInfo from '../../../components/OrderInfo';
 import "./style.scss";
 
 const OrderProcessing = () => {
-    const [classActive, setClassActive] = useState(null);
     const arr2 = ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"];
     const label = "Chuyển khoản: Tên tài khoản: PHẠM MINH CHƯƠNG \n- Vietcombank : 0451000502912 chi nhánh Thành Công \n- Techcombank: 19026463056013 chi nhánh Hà Nội \n- BIDV: xxxxxxxxxxxxxx chi nhánh Tràng An \n- Vietinbank: 100870533806 chi nhánh Chương Dương \n- Agribank: 1200209552247 Sở Giao Dịch";
     return (
         <div className="ccs-order-process-wrap">
             <div className="ccs-order-content">
-                <OrderInfo isLogin={true} />
+                <div className="ccs-order-left">
+                    <OrderInfo isLogin={false} />
+                    <div className="ccs-order-deliver">
+                        <div className="order-deliver--header">Vận chuyển & Thanh toán</div>
+                        <div className="order-deliver--option">
+                            <div className='deliver--option_title'>Hình thức vận chuyển</div>
+                            <div className='deliver--option_item'>
+                                <Radio text="Giao hàng trong 48 giờ: 0 đ" id="48h" name="radioDeliver" />
+                            </div>
+                            <div className='deliver--option_item'>
+                                <Radio text="[NowFree] Giao hàng nhanh trong 2 giờ: 9,000 đ" className="ccs-radio-disable" id="2h" name="radioDeliver" />
+                                <span className="option_item__warning">
+                                    Đơn hàng không đủ điều kiện giao nhanh 2 giờ
+                                <a href="#"> (Xem thêm)</a>
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="order-deliver--option">
+                            <div className='deliver--option_title'>Hình thức thanh toán</div>
+                            <div className='deliver--option_item'>
+                                <Radio
+                                    text="Thanh toán tiền khi nhận hàng (COD)"
+                                    id="option1"
+                                    name="radioPayment" />
+                            </div>
+                            <div className='deliver--option_item'>
+                                <Radio
+                                    text="Thẻ ATM nội địa (Internet Banking, VNPAY-QR, Ví VnMart - Miễn phí thanh toán)"
+                                    id="option2"
+                                    name="radioPayment"
+                                    className="rd-mr-0"
+                                />
+                                <a href="#"> (Xem thêm)</a>
+                            </div>
+                            <div className='deliver--option_item'>
+                                <Radio
+                                    text={label}
+                                    id="option3"
+                                    name="radioPayment"
+                                    className="option_item_radio3"
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="ccs-order-detail">
                     <div className="ccs-order-header">
                         <span>Đơn hàng</span>
@@ -79,49 +122,7 @@ const OrderProcessing = () => {
                         />
                     </div>
                 </div>
-                <div className="ccs-order-deliver">
-                    <div className="order-deliver--header">Vận chuyển & Thanh toán</div>
-                    <div className="order-deliver--option">
-                        <div className='deliver--option_title'>Hình thức vận chuyển</div>
-                        <div className='deliver--option_item'>
-                            <Radio text="Giao hàng trong 48 giờ: 0 đ" id="48h" name="radioDeliver" />
-                        </div>
-                        <div className='deliver--option_item'>
-                            <Radio text="[NowFree] Giao hàng nhanh trong 2 giờ: 9,000 đ" className="ccs-radio-disable" id="2h" name="radioDeliver" />
-                            <span className="option_item__warning">
-                                Đơn hàng không đủ điều kiện giao nhanh 2 giờ
-                                <a href="#"> (Xem thêm)</a>
-                            </span>
-                        </div>
-                    </div>
 
-                    <div className="order-deliver--option">
-                        <div className='deliver--option_title'>Hình thức thanh toán</div>
-                        <div className='deliver--option_item'>
-                            <Radio
-                                text="Thanh toán tiền khi nhận hàng (COD)"
-                                id="option1"
-                                name="radioPayment" />
-                        </div>
-                        <div className='deliver--option_item'>
-                            <Radio
-                                text="Thẻ ATM nội địa (Internet Banking, VNPAY-QR, Ví VnMart - Miễn phí thanh toán)"
-                                id="option2"
-                                name="radioPayment"
-                                className="rd-mr-0"
-                            />
-                            <a href="#"> (Xem thêm)</a>
-                        </div>
-                        <div className='deliver--option_item'>
-                            <Radio
-                                text={label}
-                                id="option3"
-                                name="radioPayment"
-                                className="option_item_radio3"
-                            />
-                        </div>
-                    </div>
-                </div>
             </div>
             <Card
                 type="horizontal"
